d69965b9557389ba2ab34751bd066ea7ddfc05d9
Leroi Dejesa & Steve Schneider <student@epicodus.com>
2015-04-27 12:24:18 -0700

    add specs for storing values, returning text for no value, returns yes or no for values have a key. add has_key method for last spec

--- lib/diy_hash.rb
+++ lib/diy_hash.rb
@@ -13,8 +13,19 @@ class DiyHash
 
   define_method(:fetch) do |key|
     key_position = @storage_array.index(key)
-    value_position = key_position+=1
-    @storage_array.at(value_position)
+    if @storage_array.include?(key)
+      value_position = key_position+=1
+      @storage_array.at(value_position)
+    else
+      "There is no value for this key"
+    end
   end
 
+  define_method(:has_key?) do |value|
+    if @storage_array.include?(value)
+      "yes"
+    else
+      "no"
+    end
+  end
 end
--- spec/diy_hash_spec.rb
+++ spec/diy_hash_spec.rb
@@ -9,4 +9,36 @@ describe(DiyHash) do
         expect(test_hash.fetch("kitten")).to(eq("cute"))
     end
   end
+
+  describe("#store") do
+    it("stores a value along with its key") do
+      test_hash = DiyHash.new()
+      expect(test_hash.store("kitten", "cute")).to(eq(["kitten", "cute"]))
+    end
+  end
+
+  describe("#fetch") do
+    it("retrieves nil when key is not present in hash") do
+        test_hash = DiyHash.new()
+        test_hash.store("kitten", "cute")
+        expect(test_hash.fetch("puppy")).to(eq("There is no value for this key"))
+    end
+  end
+
+  describe("#has_key?") do
+    it("returns 'yes' when value has a key") do
+      test_hash = DiyHash.new()
+      test_hash.store("kitten", "cute")
+      expect(test_hash.has_key?("kitten")).to(eq("yes"))
+    end
+  end
+
+describe("#has_key?") do
+  it("returns 'no' when value has a key") do
+    test_hash = DiyHash.new()
+    test_hash.store("kitten", "cute")
+    expect(test_hash.has_key?("puppy")).to(eq("no"))
+  end
+end
+
 end
